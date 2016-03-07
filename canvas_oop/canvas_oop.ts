9750d3ca3d5ce94b0801b9ce20d5f177a191f67b
LiuEV <2760295700@qq.com>
2016-03-07 14:49:11 +0800

    3

--- canvas_oop/canvas_oop.ts
+++ canvas_oop/canvas_oop.ts
@@ -46,9 +46,9 @@ class Bitmap extends DisplayObject {
 
 class Rect extends DisplayObject {
 
-    width = 500
+    width = 100
 
-    height = 500;
+    height = 100;
 
     color = '#FF0000';
 
@@ -59,11 +59,15 @@ class Rect extends DisplayObject {
 }
 
 class TextField extends DisplayObject {
+    
+    font = "20px Arial";
+    color = '#000000';
+    filltext = 'HelloWorld';
 
     render(context: CanvasRenderingContext2D) {
-        context.font = "20px Arial";
-        context.fillStyle = '#000000';
-        context.fillText('HelloWorld', 0, 20);
+        context.font = this.font;
+        context.fillStyle = this.color;
+        context.fillText(this.filltext, 0, 20);
     }
 }
 
@@ -104,29 +108,81 @@ var context = canvas.getContext("2d");
 
 
 var rect = new Rect();
-rect.width = 200;
-rect.height = 100;
-rect.color = '#00FF00'
+rect.width = 400;
+rect.height = 400;
+rect.color = '#00c7ce'
 
 
 var rect2 = new Rect();
-rect2.width = 300;
-rect2.height = 50;
-rect2.x = 200;
-rect2.y = 200;
-rect2.rotation = Math.PI / 8;
-rect2.color = '#00FFFF'
+rect2.width = 400;
+rect2.height = 150;
+rect2.x=0;
+rect2.y=250;
+rect2.color = '#00c109'
 
-var text = new TextField();
-text.x = 10;
+var rect3 = new Rect();
+rect3.width = 400;
+rect3.height = 75;
+rect3.x=0;
+rect3.y=325;
+rect3.color = '#2fb11d'
+
+var text1 = new TextField();
+text1.x = 80;
+text1.y = 80;
+text1.filltext = 'Soccer';
+text1.font = "60px Showcard Gothic";
+text1.color = '#f9aa45';
+text1.rotation = Math.PI/24*(-1);
+
+var text2 = new TextField();
+text2.x = 83;
+text2.y = 83;
+text2.filltext = 'Soccer';
+text2.font = "60px Showcard Gothic";
+text2.color = '#ffffff';
+text2.rotation = Math.PI/24*(-1);
+
+var text3 = new TextField();
+text3.x = 0;
+text3.y = 140;
+text3.filltext = 'Physics';
+text3.font = "60px Showcard Gothic";
+text3.color = "#f9aa45";
+text3.rotation = Math.PI/24*(-1);
+
+var text4 = new TextField();
+text4.x = 3;
+text4.y = 143;
+text4.filltext = 'Physics';
+text4.font = "60px Showcard Gothic";
+text4.color = "#ffffff";
+text4.rotation = Math.PI/24*(-1);
+
+var text5 = new TextField();
+text5.x = 250;
+text5.y = 160;
+text5.filltext = '2D';
+text5.font = "95px Showcard Gothic";
+text5.color = '#b1b1b1';
+text5.rotation = Math.PI/24*(-1);
+
+var text6 = new TextField();
+text6.x = 253;
+text6.y = 163;
+text6.filltext = '2D';
+text6.font = "95px Showcard Gothic";
+text6.color = '#ffffff';
+text6.rotation = Math.PI/24*(-1);
+
 
 var bitmap = new Bitmap();
-bitmap.source = '2dgame.jpg';
+bitmap.source = 'wander-icon.jpg';
 
 //渲染队列
-var renderQueue = [rect, rect2, text,bitmap];
+var renderQueue = [rect, text2, text4, text6, rect2, rect3, text1, text3, text5];
 //资源加载列表
-var imageList = ['2dgame.jpg'];
+var imageList = ['wander-icon.jpg'];
 
 //先加载资源，加载成功之后执行渲染队列
 loadResource(imageList, function() {
